75dd792d1967e7a67d0743bf5370069ea8464433
James Kelly <Jim.Kelly@StoneGateMtg.com>
2015-10-30 13:10:40 -0400

    bug fixes

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 'use strict';
 const buildAngularFile = require('./lib/baseTypes/buildAngularFile');
 
-module.exports = {
-    buildFactory: (options) => buildAngularFile.buildFunctionFile(options, 'factory'),
-    buildController: (options) => buildAngularFile.buildFunctionFile(options, 'controller'),
-    buildService: (options) => buildAngularFile.buildFunctionFile(options, 'service'),
-    buildDirective: (options) => buildAngularFile.buildFunctionFile(options, 'directive'),
-    buildConstant: (options) => buildAngularFile.buildValueFile(options, 'constant'),
-    buildValue: (options) => buildAngularFile.buildValueFile(options, 'value'),
-    buildConfig: (options) => buildAngularFile.buildFunctionFile(options, 'config'),
-    buildRoute: (options) => buildAngularFile.buildFunctionFile(options, 'route'),
-    buildProvider: (options) => buildAngularFile.buildProviderFile(options, 'provider'),
-    buildFilter: (options) => buildAngularFile.buildFilterFile(options),
-    buildModule: () => buildAngularFile.buildModuleFile(options),
-    buildModuleExports: () => buildAngularFile.buildModuleExportFile(options)
-};
+const components = new Map();
+components.set('factory', (options) => buildAngularFile.buildFunctionFile(options, 'factory'));
+components.set('controller', (options) => buildAngularFile.buildFunctionFile(options, 'controller'));
+components.set('service', (options) => buildAngularFile.buildFunctionFile(options, 'service'));
+components.set('directive', (options) => buildAngularFile.buildFunctionFile(options, 'directive'));
+components.set('constant', (options) => buildAngularFile.buildValueFile(options, 'constant'));
+components.set('value', (options) => buildAngularFile.buildValueFile(options, 'value'));
+components.set('config', (options) => buildAngularFile.buildConfigFile(options, 'config'));
+components.set('run', (options) => buildAngularFile.buildConfigFile(options, 'config'));
+components.set('route', (options) => buildAngularFile.buildConfigFile(options, 'route'));
+components.set('provider', (options) => buildAngularFile.buildProviderFile(options, 'provider'));
+components.set('filter', (options) => buildAngularFile.buildFilterFile(options));
 
+module.exports = {
+    buildFactory: (options) => components.get('factory')(options),
+    buildController: (options) => components.get('controller')(options),
+    buildService: (options) => components.get('service')(options),
+    buildDirective: (options) => components.get('directive')(options),
+    buildConstant: (options) => components.get('constant')(options),
+    buildValue: (options) => components.get('value')(options),
+    buildConfig: (options) => components.get('config')(options),
+    buildRun: (options) => components.get('run')(options),
+    buildRoute: (options) => components.get('route')(options),
+    buildProvider: (options) => components.get('provider')(options),
+    buildFilter: (options) => components.get('filter')(options),
+    buildModule: (options) => buildAngularFile.buildModuleFile(options),
+    buildModuleExports: (options) => buildAngularFile.buildModuleExportFile(options),
+    buildComponent: (type, options) => {
+        if(components.has(type)) {
+            return components.get(type)(options);
+        }
+        throw new Error(`type ${type} not a defined component.`);
+    }
+};
